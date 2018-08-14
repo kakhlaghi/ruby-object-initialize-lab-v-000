6cbf3f3c80351a7933bcb84b0ac9fdfcb1b316fe
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-08-14 13:55:33 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+  def initialize(breed)
+    @breed = breed
+  end
+  def name=(name)
+    @name
+  end
+  def breed=(breed)
+    @breed
+  end
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end
+end
+  
